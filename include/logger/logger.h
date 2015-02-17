7d808ca2c8072949efb4a72ff3821d3de7c52d83
Guillaume Plum <guiplum@gmail.com>
2015-02-17 13:34:19 +0100

    Add a logger object.

---
+++ include/logger/logger.h
@@ -0,0 +1,42 @@
+#ifndef LOGGER_H_7FKXRP9Q
+#define LOGGER_H_7FKXRP9Q
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef enum _Level {
+	ERROR=0,
+	WARN,
+	INFO,
+	DEBUG,
+	ALL
+} Level;
+
+typedef struct _Logger {
+	FILE *output;
+	enum _Level level;
+
+	bool dealloc_out,
+	     dealloc_error,
+	     dealloc_warn,
+	     dealloc_debug,
+	     dealloc_info;
+
+	char *error,
+	     *warn,
+	     *debug,
+	     *info;
+} Logger;
+
+Logger* Logger_new(FILE *out, enum _Level level);
+void Logger_free(Logger *log);
+
+int Logger_info(Logger *log, const char *str, ...);
+int Logger_error(Logger *log, const char *str, ...);
+int Logger_warn(Logger *log, const char *str, ...);
+int Logger_debug(Logger *log, const char *str, ...);
+
+#endif /* end of include guard: LOGGER_H_7FKXRP9Q */
---
+++ src/logger/logger.c
@@ -0,0 +1,163 @@
+#include "logger/logger.h"
+
+static const char *def_error = "\033[31m[ERROR]\033[00m ";
+static const char *def_warn = "\033[33m[WARNING]\033[00m ";
+static const char *def_debug = "\033[36m[DEBUG]\033[00m ";
+static const char *def_info = "\033[32m[INFO]\033[00m ";
+
+Logger* Logger_new(FILE *out, enum _Level level)
+{
+	Logger *log = NULL;
+
+	if( (log = malloc(sizeof(struct _Logger))) == NULL )
+	{
+		perror("Error while allocating the logger");
+		return NULL;
+	}
+
+	log->output = out;
+	log->level = level;
+
+	log->error = def_error;
+	log->info = def_info;
+	log->debug = def_debug;
+	log->warn = def_warn;
+
+	log->dealloc_error = false;
+	log->dealloc_info = false;
+	log->dealloc_debug = false;
+	log->dealloc_warn = false;
+	log->dealloc_out = false;
+
+	return log;
+}
+
+void Logger_free(Logger *log)
+{
+	if( log->dealloc_error && log->error != NULL )
+		free(log->error);
+
+	if( log->dealloc_warn && log->warn != NULL )
+		free(log->warn);
+
+	if( log->dealloc_debug && log->debug != NULL )
+		free(log->debug);
+
+	if( log->dealloc_info && log->info != NULL )
+		free(log->info);
+
+	if( log->dealloc_out )
+		fclose(log->output);
+
+	free(log);
+}
+
+int print_with_prompt(FILE *out, const char *ppt, const char *str, va_list va)
+{
+	int ret;
+	size_t s_len = strlen(str), p_len = strlen(ppt);
+	char *fmt = NULL;
+
+	if( (fmt = malloc((s_len + p_len + 1) * sizeof(char))) == NULL )
+	{
+		perror("Error while allocating the logger");
+		return -1;
+	}
+
+	strcpy(fmt, ppt);
+	strncat(fmt, str, s_len);
+
+	ret = vfprintf(
+		out,
+		fmt,
+		va
+	);
+
+	free(fmt);
+	return ret;
+}
+
+int Logger_error(Logger *log, const char *str, ...)
+{
+	if( log->level < ERROR )
+		return 0;
+
+	int ret;
+	va_list va;
+	va_start(va, str);
+
+	ret = print_with_prompt(
+		log->output,
+		log->error,
+		str,
+		va
+	);
+
+	va_end(va);
+
+	return ret;
+}
+
+int Logger_warn(Logger *log, const char *str, ...)
+{
+	if( log->level < WARN )
+		return 0;
+
+	int ret;
+	va_list va;
+	va_start(va, str);
+
+	ret = print_with_prompt(
+		log->output,
+		log->warn,
+		str,
+		va
+	);
+
+	va_end(va);
+
+	return ret;
+}
+
+int Logger_debug(Logger *log, const char *str, ...)
+{
+	if( log->level < DEBUG )
+		return 0;
+
+	int ret;
+	va_list va;
+	va_start(va, str);
+
+	ret = print_with_prompt(
+		log->output,
+		log->debug,
+		str,
+		va
+	);
+
+	va_end(va);
+
+	return ret;
+}
+
+int Logger_info(Logger *log, const char *str, ...)
+{
+	if( log->level < INFO )
+		return 0;
+
+	int ret;
+	va_list va;
+	va_start(va, str);
+
+	ret = print_with_prompt(
+		log->output,
+		log->info,
+		str,
+		va
+	);
+
+	va_end(va);
+
+	return ret;
+}
+
