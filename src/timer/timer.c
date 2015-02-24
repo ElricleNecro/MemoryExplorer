d1f19a11920c3dab60619000197b181a0ee4026b
Guillaume Plum <guiplum@gmail.com>
2015-02-24 23:32:27 +0100

    Add some variable to have a stack and a heap, so a more complete memory structure.

--- src/timer/timer.c
+++ src/timer/timer.c
@@ -1,12 +1,23 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	int stop = 5, a = 0;
+	double *d = malloc(sizeof(double));
+
+	*d = 5.;
 
 	fprintf(stderr, "(%d) need a=%d to stop!\n", getpid(), stop);
 	fprintf(stderr, "%p, %p\n", &stop, &a);
+	fprintf(stderr, "%p, %p, %g\n", &d, d, *d);
 
 	while(a != stop) sleep(1);
+
+	fprintf(stderr, "%g, %d, %d\n", *d, a,stop);
+
+	free(d);
+
+	return 0;
 }
