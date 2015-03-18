c0e26e01e7d160d72a2c4e85cd1403e099c6488c
Guillaume Plum <guiplum@gmail.com>
2015-03-18 18:50:19 +0100

    Add my own lua binding for the scan function.

--- src/hack/lua_decl.c
+++ src/hack/lua_decl.c
@@ -35,6 +35,88 @@ static int C(lua_State *L)
 	return luaA_call_name(L, lua_tostring(L, 1));
 }
 
+int Mylua_Scan(lua_State *L)
+{
+	Event *ev;
+	int size = lua_gettop(L);
+	void *out;
+	const char *str;
+	unsigned long addr, to_read;
+
+	if( size != 4 )
+	{
+		lua_pushstring(L, "Invalid number of argument.");
+		lua_error(L);
+
+		return 0;
+	}
+
+	luaA_to_pEvent(L, luaA_type_find(L, "pEvent"), &ev, 1);
+
+	/* if( !lua_isnumber(L, 2) ) */
+	/* { */
+		/* lua_pushstring(L, "Incorrect argument type"); */
+		/* lua_error(L); */
+
+		/* return 0; */
+	/* } */
+	Logger_debug(
+		ev->log,
+		"Arg %d: '%s'",
+		2,
+		lua_tostring(L, 2)
+	);
+	addr = luaL_checknumber(L, 2);
+
+	/* if( !lua_isnumber(L, 3) ) */
+	/* { */
+		/* lua_pushstring(L, "Incorrect argument type"); */
+		/* lua_error(L); */
+
+		/* return 0; */
+	/* } */
+	Logger_debug(
+		ev->log,
+		"Arg %d: '%s'",
+		3,
+		lua_tostring(L, 3)
+	);
+	to_read = luaL_checknumber(L, 3);
+
+	Logger_debug(
+		ev->log,
+		"Address to read: '0x%lx'\n",
+		addr
+	);
+	Logger_debug(
+		ev->log,
+		"Bytes to read: '%ld'\n",
+		to_read
+	);
+
+	if( !scan(ev, addr, to_read, &out) )
+	{
+		lua_pushstring(L, "Function 'scan' exited without finishing it's task");
+		lua_error(L);
+		return 0;
+	}
+
+	str = luaL_checkstring(L, 4);
+
+	if( strcmp(str, "int") )
+		lua_pushnumber(L, (lua_Number)*(int*)out);
+	else if( strcmp(str, "long") )
+		lua_pushnumber(L, (lua_Number)*(long*)out);
+	else if( strcmp(str, "float") )
+		lua_pushnumber(L, (lua_Number)*(float*)out);
+	else if( strcmp(str, "double") )
+		lua_pushnumber(L, (lua_Number)*(double*)out);
+
+	free(out);
+
+	return 1;
+}
+
 lua_State* lua_Init(void)
 {
 	lua_State *L = luaL_newstate();
@@ -48,6 +130,9 @@ lua_State* lua_Init(void)
 
 	lua_register(L, "C_call", C);
 
+	lua_pushcfunction(L, Mylua_Scan);
+	lua_setglobal(L, "Scanner");
+
 	lua_Event(L);
 
 	return L;
@@ -55,7 +140,7 @@ lua_State* lua_Init(void)
 
 luaA_function_declare(quit, bool, Event*)
 luaA_function_declare(print_map, bool, Event*)
-luaA_function_declare(scan, bool, Event*, size_t, ssize_t)
+/* luaA_function_declare(scan, bool, Event*, unsigned long, unsigned long) */
 
 void lua_Event(lua_State *L)
 {
@@ -66,6 +151,6 @@ void lua_Event(lua_State *L)
 
 	luaA_function_register(L, quit, bool, Event*);
 	luaA_function_register(L, print_map, bool, Event*);
-	luaA_function_register(L, scan, bool, Event*, unsigned long, unsigned long);
+	/* luaA_function_register(L, scan, bool, Event*, unsigned long, unsigned long); */
 }
 
