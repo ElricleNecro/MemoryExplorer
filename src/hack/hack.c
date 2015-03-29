377ef8c3020ab9c76da6267337b1ad7146b40876
Guillaume Plum <guiplum@gmail.com>
2015-03-29 15:49:19 +0200

    Add a log of debug instruction. Finished to adapt the readv version.

--- src/hack/hack.c
+++ src/hack/hack.c
@@ -40,7 +40,23 @@ bool scan(Event *ev, size_t offset, ssize_t bytes_to_read, void *out)
 	/* lseek(ev->mem_fd, offset, SEEK_SET); */
 	/* read(ev->mem_fd, buf, bytes_to_read); */
 	pread(ev->mem_fd, buf, bytes_to_read, offset);
-	fprintf(stderr, "%ld\n",
+	fprintf(stderr, "ptrace %ld\n",
+		ptrace(
+			PTRACE_PEEKDATA,
+			ev->pid,
+			offset,
+			NULL
+		)
+	);
+	fprintf(stderr, "ptrace %g\n",
+		ptrace(
+			PTRACE_PEEKDATA,
+			ev->pid,
+			offset,
+			NULL
+		)
+	);
+	fprintf(stderr, "Complement avec ptrace %ld\n",
 		~ptrace(
 			PTRACE_PEEKDATA,
 			ev->pid,
@@ -49,9 +65,10 @@ bool scan(Event *ev, size_t offset, ssize_t bytes_to_read, void *out)
 		) + 1
 	);
 
-#define cad(var) ((~var) + 1)
-
-	Logger_info(ev->log, "Value as int: '%d'\n", cad( *((int*)buf) ));
+	Logger_info(ev->log, "Value as int (2complements): '%d'\n", cad( *((int*)( (void*)buf )) ));
+	Logger_info(ev->log, "Value as int (bswap64): '%d'\n", bswap_64( *((int*)( (void*)buf )) ));
+	Logger_info(ev->log, "Value as int (~bswap64+1): '%d'\n", cad( bswap_64( *((int*)( (void*)buf )) ) ));
+	Logger_info(ev->log, "Value as int (~bswap64+1): '%d'\n", bswap_64( cad( *((int*)( (void*)buf )) ) ));
 
 	ptrace(
 		PTRACE_DETACH,
@@ -79,10 +96,19 @@ bool scan(Event *ev, size_t offset, ssize_t bytes_to_read, void *out)
 	/* } */
 	Logger_info(ev->log, "Reading %zu bytes from 0x%zx for pid(%d)\n", bytes_to_read, offset, ev->pid);
 
-	char buf[bytes_to_read];
+	char *buf = NULL;
 
 	struct iovec local, remote;
 
+	if( (buf = malloc(bytes_to_read)) == NULL )
+	{
+		Logger_error(
+			ev->log,
+			"Allocation error: '%s'\n",
+			strerror(errno)
+		);
+		return false;
+	}
 
 	local.iov_base = buf;
 	local.iov_len  = bytes_to_read;
@@ -101,14 +127,27 @@ bool scan(Event *ev, size_t offset, ssize_t bytes_to_read, void *out)
 
 	if( nread != bytes_to_read )
 	{
-		if( errno == 1 )
-			Logger_error(ev->log, "operation not permitted");
-		else if( errno == 14 )
-			Logger_error(ev->log, "BAD ADDRESS");
-		else
-			Logger_error(ev->log, "read %zd instead of %zu. Error code %d", nread, bytes_to_read, errno);
+		Logger_error(
+			ev->log,
+			"Allocation error: '%s'\n",
+			strerror(errno)
+		);
 		return false;
+		/* if( errno == 1 ) */
+			/* Logger_error(ev->log, "operation not permitted"); */
+		/* else if( errno == 14 ) */
+			/* Logger_error(ev->log, "BAD ADDRESS"); */
+		/* else */
+			/* Logger_error(ev->log, "read %zd instead of %zu. Error code %d", nread, bytes_to_read, errno); */
+		/* return false; */
 	}
+
+	Logger_info(ev->log, "Value as int (2complements): '%d'\n", cad( *((int*)( (void*)buf )) ));
+	Logger_info(ev->log, "Value as int (bswap64): '%d'\n", bswap_64( *((int*)( (void*)buf )) ));
+	Logger_info(ev->log, "Value as int (~bswap64+1): '%d'\n", cad( bswap_64( *((int*)( (void*)buf )) ) ));
+	Logger_info(ev->log, "Value as int (~bswap64+1): '%d'\n", bswap_64( cad( *((int*)( (void*)buf )) ) ));
+
+	*(char**)out = buf;
 
 	return true;
 }
