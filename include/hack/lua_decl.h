9921a19169ab81129049e847d17a5afabae6ff85
Guillaume Plum <guiplum@gmail.com>
2015-04-01 00:33:02 +0200

    Add two convienient macro to get float and double from LUA.

--- include/hack/lua_decl.h
+++ include/hack/lua_decl.h
@@ -10,6 +10,13 @@
 
 #include "hack/hack.h"
 
+#ifndef luaL_checkfloat
+#define luaL_checkfloat(L, n) ((float)luaL_checknumber(L, (n)))
+#endif
+#ifndef luaL_checkdouble
+#define luaL_checkdouble(L, n) ((double)luaL_checknumber(L, (n)))
+#endif
+
 lua_State* lua_Init(void);
 void lua_Event(lua_State *L);
 
