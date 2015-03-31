55ebbc7a149b8c8d683363ecd18eafbc8eec364b
Guillaume Plum <guiplum@gmail.com>
2015-04-01 00:33:44 +0200

    Add the LUA binding for the new hack function and export it.

--- src/hack/lua_decl.c
+++ src/hack/lua_decl.c
@@ -119,6 +119,124 @@ int Mylua_Scan(lua_State *L)
 	return 1;
 }
 
+int Mylua_Write(lua_State *L)
+{
+	if( lua_gettop(L) != 5 )
+	{
+		lua_pushstring(L, "Invalid number of argument.");
+		lua_error(L);
+
+		return 0;
+	}
+
+	Event *ev;
+	void *in;
+	const char *str;
+	unsigned long addr = luaL_checklong(L, 2), to_write = luaL_checklong(L, 3);
+
+	// There may be a bug in this function call or around:
+	luaA_to_pEvent(L, luaA_type_find(L, "pEvent"), &ev, 1);
+
+	Logger_debug(
+		ev->log,
+		"Address to write: '0x%lx'\n",
+		addr
+	);
+	Logger_debug(
+		ev->log,
+		"Bytes to write: '%ld'\n",
+		to_write
+	);
+
+	str = luaL_checkstring(L, 4);
+
+	if( !strcmp(str, "int") )
+	{
+		if( (in = malloc(sizeof(int))) == NULL )
+		{
+			Logger_error(
+				ev->log,
+				"Allocation error: '%s'\n",
+				strerror(errno)
+			);
+		}
+		Logger_debug(ev->log, "'%s' -- > '%d'\n", lua_tostring(L, 5), luaL_checkint(L, 5));
+		*((int*)in) = luaL_checkint(L, 5);
+		Logger_debug(
+			ev->log,
+			"Going to write '%d'\n",
+			*((int*)in)
+		);
+	}
+	else if( !strcmp(str, "long") )
+	{
+		if( (in = malloc(sizeof(long))) == NULL )
+		{
+			Logger_error(
+				ev->log,
+				"Allocation error: '%s'\n",
+				strerror(errno)
+			);
+		}
+		*((long*)in) = luaL_checklong(L, 5);
+		Logger_debug(
+			ev->log,
+			"Going to write '%ld'\n",
+			*((long*)in)
+		);
+	}
+	else if( !strcmp(str, "float") )
+	{
+		if( (in = malloc(sizeof(float))) == NULL )
+		{
+			Logger_error(
+				ev->log,
+				"Allocation error: '%s'\n",
+				strerror(errno)
+			);
+		}
+		*((float*)in) = luaL_checkfloat(L, 5);
+		Logger_debug(
+			ev->log,
+			"Going to write '%g'\n",
+			*((float*)in)
+		);
+	}
+	else if( !strcmp(str, "double") )
+	{
+		if( (in = malloc(sizeof(double))) == NULL )
+		{
+			Logger_error(
+				ev->log,
+				"Allocation error: '%s'\n",
+				strerror(errno)
+			);
+		}
+		*((double*)in) = luaL_checkdouble(L, 5);
+		Logger_debug(
+			ev->log,
+			"Going to write '%g'\n",
+			*((double*)in)
+		);
+	}
+	else
+	{
+		lua_pushstring(L, "No valid type were given!");
+		lua_error(L);
+		return 0;
+	}
+
+	if( !Event_write(ev, addr, to_write, in) )
+	{
+		lua_pushstring(L, "Function 'write' exited without finishing it's task");
+		lua_error(L);
+	}
+
+	free(in);
+
+	return 0;
+}
+
 lua_State* lua_Init(void)
 {
 	lua_State *L = luaL_newstate();
@@ -134,6 +252,9 @@ lua_State* lua_Init(void)
 
 	lua_pushcfunction(L, Mylua_Scan);
 	lua_setglobal(L, "Scanner");
+
+	lua_pushcfunction(L, Mylua_Write);
+	lua_setglobal(L, "Writer");
 
 	lua_Event(L);
 
