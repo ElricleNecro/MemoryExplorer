f3d2fbb624fdedbab4a5d3de13d3d370790bb2ea
Guillaume Plum <guiplum@gmail.com>
2015-07-06 17:08:41 +0200

    Adapt to the new LUA interface.

--- init.lua
+++ init.lua
@@ -1,47 +1,14 @@
-Event = {}
-setmetatable(Event, Event)
-function Event.__call()
-	local self = {}
-	setmetatable(self, Event)
-	return self
-end
-
-Event.__index = Event_index
-Event.__newindex = Event_newindex
-
-ev = Event()
-
-function Lquit()
-	ev.quit = true
-end
-
-CBind = {}
-setmetatable(CBind, CBind)
-function CBind.__call()
-	local self = {}
-	setmetatable(self, CBind)
-	return self
-end
-
-CBind.__index = function(table, key)
-	return function(...)
-		return C_call(key, ...)
-	end
-end
-
-C = CBind()
-
 function test_modif(a_addr, stop_addr)
-	a_value = Scanner(ev, a_addr, 4, "int")
-	stop_value = Scanner(ev, stop_addr, 4, "int")
+	a_value = ev:scan(a_addr, 4, "int")
+	stop_value = ev:scan(stop_addr, 4, "int")
 
 	print("a = ", a_value)
 	print("stop = ", stop_value)
 
 	print("setting a to ", stop_value - 2)
 
-	Writer(ev, a_addr, 4, "int", stop_value - 2)
+	ev:write(a_addr, 4, "int", stop_value - 2)
 
-	return Scanner(ev, a_addr, 4, "int")
+	return ev:scan(a_addr, 4, "int")
 end
 
