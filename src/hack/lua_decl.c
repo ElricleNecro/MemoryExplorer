00c197d62424e5e3a751611478d350841f6c168d
Guillaume Plum <guiplum@gmail.com>
2015-08-15 12:28:42 +0200

    Now checking LUA error when parsing the configuration file.

--- src/hack/lua_decl.c
+++ src/hack/lua_decl.c
@@ -11,7 +11,8 @@ lua_State* MyLua_Init(const char *conf, Event *ev, bool *quit)
 	lua_setglobal(L, "Event");
 
 	if( conf )
-		luaL_dofile(L, conf);
+		if( luaL_dofile(L, conf) )
+			Logger_Error(ev->log, "Unable do run file '%s': '%s'\n", conf, lua_tostring(L, -1));
 
 	bool_create_from_c(L, quit);
 	event_create_from_c(L, ev);
