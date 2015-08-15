f51e4221e8f75a9bfe26f9160dae478ea42f0044
Guillaume Plum <guiplum@gmail.com>
2015-08-15 12:28:02 +0200

    Add a LUA helper function to create the map of the process.

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 function test_modif(a_addr, stop_addr)
+	local a_value, stop_value
+
 	a_value = ev:scan(a_addr, 4, "int")
 	stop_value = ev:scan(stop_addr, 4, "int")
 
@@ -12,3 +14,17 @@ function test_modif(a_addr, stop_addr)
 	return ev:scan(a_addr, 4, "int")
 end
 
+function initialize(a_addr, s_addr, d_addr)
+	local stack = {}
+	local heap = {}
+	local code = {}
+	local var = {}
+
+	stack.start, stack.fin = ev:get_map("stack")
+	heap.start, heap.fin = ev:get_map("heap")
+	code.start, code.fin = ev:get_map("code")
+
+	var.a, var.s, var.d = a_addr, s_addr, d_addr
+
+	return stack, heap, code, var
+end
