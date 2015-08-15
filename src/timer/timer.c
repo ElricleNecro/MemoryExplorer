0d4429e9619f6763ac9f3ccdaa2ac2fe00d6f6dd
Guillaume Plum <guiplum@gmail.com>
2015-08-15 12:30:06 +0200

    Stoping printing cli args. Add a new test variable.

--- src/timer/timer.c
+++ src/timer/timer.c
@@ -4,18 +4,19 @@
 
 int main(int argc, const char **argv)
 {
-	printf("argc == %d\n", argc);
-	for(int i=0; i<argc; i++)
-		printf("%d -> '%s'\n", i, argv[i]);
+	/* printf("argc == %d\n", argc); */
+	/* for(int i=0; i<argc; i++) */
+		/* printf("%d -> '%s'\n", i, argv[i]); */
 
 	int stop = 5, a = 0;
+	double c = 60.;
 	double *d = malloc(sizeof(double));
 
 	*d = 5.;
 
 	fprintf(stderr, "(%d) need a=%d to stop!\n", getpid(), stop);
-	fprintf(stderr, "&stop = %p, &a = %p\n", &stop, &a);
-	fprintf(stderr, "stop = %d, a = %d\n", stop, a);
+	fprintf(stderr, "&stop = %p, &a = %p, &c = %p\n", &stop, &a, &c);
+	fprintf(stderr, "stop = %d, a = %d, c = %g\n", stop, a, c);
 	fprintf(stderr, "&d = %p, d = %p, *d = %g\n", &d, d, *d);
 
 	while(a != stop) sleep(1);
